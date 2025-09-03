8f2944bb15ab319543d165e01d2b5dac45964a77
NohammadFaijAnsai <mohammadfaijansari6@gmail.com>
2025-09-03 16:10:20 +0530

    main

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,10 +1,16 @@
 const { Sequelize } = require("sequelize");
+require("dotenv").config(); // load .env
 
-const sequelize = new Sequelize("taskmanager", "root", "faij123", {
-  host: "localhost",
-  dialect: "mysql",
-  port: 3307,
-  logging: false,
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    port: process.env.DB_PORT || 3306,
+    logging: false,
+  }
+);
 
 module.exports = sequelize;
